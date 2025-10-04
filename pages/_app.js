65904a773fc9a2df676c34aa39d8f0c3ee48e4b1
0xmeaLone <0xmealone@gmail.com>
2025-10-04 16:42:28 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
-// pages/_app.jsx
-// MENGGUNAKAN JALUR ROOT PROJECT UNTUK KEAMANAN BUILD
-import '../styles/globals.css'; 
+// File: pages/_app.js
+import '../styles/globals.css'; // MENGGUNAKAN JALUR INI!
 
-// Ini adalah komponen yang membungkus semua halaman (index, submit, [slug])
 function MyApp({ Component, pageProps }) {
-  // Hanya me-render komponen halaman saat ini
   return <Component {...pageProps} />;
 }
 
-// WAJIB: Export default untuk Next.js
 export default MyApp;
