2b07992a962f8080c1fb5bd95048379527ade4c7
0xmeaLone <0xmealone@gmail.com>
2025-10-04 16:34:45 +0900

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// File: postcss.config.js
+
+module.exports = {
+  plugins: {
+    // 1. Tambahkan Tailwind CSS sebagai plugin pertama
+    tailwindcss: {},
+    // 2. Tambahkan Autoprefixer untuk kompatibilitas browser
+    autoprefixer: {},
+  },
+};
