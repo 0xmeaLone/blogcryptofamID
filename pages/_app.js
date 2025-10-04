c7e8a67a13017179619ea95a22be4fca4ca18697
0xmeaLone <0xmealone@gmail.com>
2025-10-04 15:44:03 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-// pages/_app.js
-// Next.js memerlukan impor CSS global HANYA di file _app.js.
+// pages/_app.jsx
+// TENTUKAN PATH CSS DARI FOLDER PAGES: '../styles/globals.css'
 import '../styles/globals.css';
-// Impor Head tidak diperlukan di sini jika sudah ada di halaman index.js
 
+// Ini adalah komponen yang membungkus semua halaman (index, submit, [slug])
 function MyApp({ Component, pageProps }) {
-  // Hanya me-render komponen halaman saat ini
+  
+  // Pastikan Anda hanya me-return satu komponen.
+  // Component adalah halaman saat ini, pageProps adalah data halaman.
   return <Component {...pageProps} />;
 }
 
+// WAJIB: Export default untuk Next.js
 export default MyApp;
