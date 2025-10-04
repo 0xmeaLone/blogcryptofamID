c6bbefb05664d62bbc029da1ec172c83998caafd
0xmeaLone <0xmealone@gmail.com>
2025-10-04 15:28:24 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,20 @@
 // pages/_app.js
+// PENTING: Next.js memerlukan impor CSS global HANYA di file _app.js.
 import '../styles/globals.css';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Head untuk setting global seperti viewport */}
       <Head>
+        {/* Memastikan desain responsif di semua perangkat */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Blog Komunitas Publik</title>
       </Head>
-      {/* Component akan menjadi halaman yang sedang dikunjungi (index, submit, [slug]) */}
+      {/* Component adalah halaman yang sedang dimuat (Home, Submit, Article Detail) */}
       <Component {...pageProps} />
     </>
   );
 }
 
-export default MyApp;
-
+export default MyApp; // Wajib di export
